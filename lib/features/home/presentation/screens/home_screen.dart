8881169740982dff0fcd1a53ef5def501fd165f5
Mohamed Gamal <mohamedgamalfcis@gmail.com>
2025-07-12 19:14:14 +0300

    test commit

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -68,6 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       fontWeight: FontWeight.w300,
                     ),
                   ),
+
                 ],
               ),
             ],
